caed1a2a535a1d6b9c4d4c785b3bb6c7690e3ece
JSheleg <jessica.sheleg@gmail.com>
2021-07-14 16:22:48 -0500

    fetch requests

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+//file path to location and instruct the server to make these files static resourses
+//all front end code can be access via public folder
+app.use(express.static('public'));
 
 
 
@@ -136,6 +139,27 @@ app.post('/api/animals', (req, res) => {
   // req.body is where our incoming content will be
   // console.log(req.body);
   res.json(animal);
+});
+
+//add route to index.html and respond with HTML page displayed in browser
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+//route to /animals
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+//route to zookeepers
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+//wildcard route, recieve homepage as a response for unknown/undefined page route. 
+//this should always be the last route
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 //set up server
